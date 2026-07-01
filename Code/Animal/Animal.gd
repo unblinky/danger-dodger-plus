1185cc6855f769fdad46fcbd938ce5bc5521279e
unblinky <>
2026-07-01 16:51:34 -0700

    Animal lerping. Reoriented Lane and Vehicle along the -Z (forward) axis.

--- Code/Animal/Animal.gd
+++ Code/Animal/Animal.gd
@@ -1,19 +1,62 @@
 extends Node3D
 class_name Animal
 
-var leap_distance = 1.0
+
+# The secret hand shake.
+# Hold Ctrl + Shift + Drop
+@onready var graphics: Node3D = $Graphics
+
+var leap_distance: float = 1.0
+var weight: float = 1.0 # 0 - 1
+var weight_speed: float = 5.0 # per sec.
+
+var current_spot: Vector3
+var next_spot: Vector3
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+# lerp(a, b, 0.5)
+#     16, 8, 0.5 = 4
+# lerp(position, next_spot, 0.25)
+
+func _ready() -> void:
+	current_spot = position
+	next_spot = position
+
+
+
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("move_left"):
-		position.x -= leap_distance
+		# position.x -= leap_distance
+		next_spot = current_spot + Vector3.LEFT
+		weight = 0.0
+		graphics.rotation_degrees.y = 90.0
 	
 	if Input.is_action_just_pressed("move_right"):
-		position.x += leap_distance
+		#position.x += leap_distance
+		next_spot = current_spot + Vector3.RIGHT
+		weight = 0.0
+		graphics.rotation_degrees.y = -90.0
 	
 	if Input.is_action_just_pressed("move_up"):
-		position.z -= leap_distance # Forward.
+		#position.z -= leap_distance # Forward.
+		next_spot = current_spot + Vector3.FORWARD
+		weight = 0.0
+		graphics.rotation_degrees.y = 0.0
 	
 	if Input.is_action_just_pressed("move_down"):
-		position.z += leap_distance # Backward. 
+		#position.z += leap_distance # Backward. 
+		next_spot = current_spot + Vector3.BACK
+		weight = 0.0
+		graphics.rotation_degrees.y = 180.0
+	
+	# TESTING:
+	# Tweening.
+	if weight < 1.0:
+		weight += weight_speed * delta
+	# Resting.
+	else:
+		weight = 1.0
+		current_spot = next_spot
+	
+	print(weight)
+	position = lerp(current_spot, next_spot, weight)
--- Code/Vehicle/Vehicle.gd
+++ Code/Vehicle/Vehicle.gd
@@ -2,9 +2,11 @@ extends Node3D
 class_name Vehicle
 
 var speed: float = 3.0 # m/s.
-var kill_distance: float = 25.0
+
+## Meters in front of car. Clamp the vehicle's loop distance traved.
+var kill_distance: float = 10.0
 
 func _process(delta: float) -> void:
-	position.x += speed * delta
-	if position.x > kill_distance:
-		position.x = -kill_distance
+	position.z -= speed * delta # Forward.
+	if position.z < -kill_distance:
+		position.z = kill_distance
